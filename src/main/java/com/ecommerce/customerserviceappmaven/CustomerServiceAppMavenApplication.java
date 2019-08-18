d328442474fc324a9c5bf77fc1c4b8d4137a6eff
Singh <Saurabh.Singh@fiserv.co.in>
2019-08-18 11:01:35 +0530

    Initial Commit

---
+++ src/main/java/com/ecommerce/customerserviceappmaven/CustomerServiceAppMavenApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.customerserviceappmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceAppMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceAppMavenApplication.class, args);
+	}
+
+}
